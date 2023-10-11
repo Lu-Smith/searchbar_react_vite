b768392b1931671143e92066c2658e99878943d3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-11 22:58:54 +0100

    Updated content of App.tsx

--- react-search-bar/src/App.tsx
+++ react-search-bar/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
-
 function App() {
 
   return (
     <div className='App'>
-      Hello World!
+      <div className="searchBarContainer">
+        <div>SearchBar</div>
+        <div>SearchResults</div>
+      </div>
     </div>
   )
 }
