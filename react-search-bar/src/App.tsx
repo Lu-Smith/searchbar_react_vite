4697bf509433547f448aea4a507586e5443b9104
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-12 22:55:41 +0100

    Updated logic in App.tsx

--- react-search-bar/src/App.tsx
+++ react-search-bar/src/App.tsx
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import SearchBar from "./components/SearchBar";
 
 function App() {
 
+  const [results, setResults] = useState([])
+
   return (
     <div className='App'>
       <div className="searchBarContainer">
-        <SearchBar />
-        <div>SearchResults</div>
+        <SearchBar setResults={setResults} />
+        <div>{results}</div>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
