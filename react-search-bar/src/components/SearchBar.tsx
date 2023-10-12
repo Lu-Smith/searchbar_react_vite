2de90ddb552c7a0dd38206d61f40b1baf01ec326
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-12 22:18:36 +0100

    Updated logic in SearchBar

--- react-search-bar/src/components/SearchBar.tsx
+++ react-search-bar/src/components/SearchBar.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = () => {
+    const [input, setInput] = useState("");
+
   return (
     <div className='input-wrapper'>
         <FaSearch id="search-icon" />
-        <input type="text" placeholder='Type to search...' />
+        <input 
+        type="text" 
+        placeholder='Type to search...' 
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
     </div>
   )
 }
