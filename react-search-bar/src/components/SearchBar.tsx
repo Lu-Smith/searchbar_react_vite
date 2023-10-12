e68ecdd18c87b78458cbd26a7e1ef8b06713e1f8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-12 22:36:07 +0100

    Updated logic in SearchBar

--- react-search-bar/src/components/SearchBar.tsx
+++ react-search-bar/src/components/SearchBar.tsx
@@ -4,6 +4,17 @@ import { FaSearch } from 'react-icons/fa';
 const SearchBar = () => {
     const [input, setInput] = useState("");
 
+    const fetchData = (value: string) => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((json) => {console.log(json)});
+    };
+
+    const handleChange = (value: string) => {
+        setInput(value);
+        fetchData(value);
+    }
+
   return (
     <div className='input-wrapper'>
         <FaSearch id="search-icon" />
@@ -11,7 +22,7 @@ const SearchBar = () => {
         type="text" 
         placeholder='Type to search...' 
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => handleChange(e.target.value)}
         />
     </div>
   )
