9b38861384e01981f50e1ddb608ed59c2baffbc4
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-13 16:14:09 +0100

    Added SearchResults to App.tsx

--- react-search-bar/src/App.tsx
+++ react-search-bar/src/App.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import SearchBar from "./components/SearchBar";
+import SearchResults from './components/SearchResults';
 
 function App() {
 
-  const [results, setResults] = useState([])
+  const [results, setResults] = useState<[]>([])
 
   return (
     <div className='App'>
       <div className="searchBarContainer">
         <SearchBar setResults={setResults} />
-        <div>{results}</div>
+        <SearchResults results={results} />
       </div>
     </div>
   )
