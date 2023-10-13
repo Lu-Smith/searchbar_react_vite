0cb711d1b2f3765385ab4beec399be4fdc246891
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-13 17:47:49 +0100

    Updated Footer.tsx content

--- react-search-bar/src/components/Footer.tsx
+++ react-search-bar/src/components/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <h4>Coded by <a href="">Luna Smith</a> - <a href="https://github.com/Lu-Smith/searchbar_react_vite"> OPEN SOURCE</a>.</h4>
+        <h4>Coded by 
+            <a href="https://www.lunasmithart.com/" target='_blank' rel="noreferrer">Luna Smith</a> - 
+            <a href="https://github.com/Lu-Smith/searchbar_react_vite" target='_blank' rel="noreferrer"> OPEN SOURCE</a>.</h4>
         <button>Click me</button>
     </div>
   )
