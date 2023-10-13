170a145118d9e2336edf799e4670bacad3094557
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-13 16:34:25 +0100

    Updated App.tsx

--- react-search-bar/src/App.tsx
+++ react-search-bar/src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react';
 import SearchBar from "./components/SearchBar";
 import SearchResults from './components/SearchResults';
 
+interface AppProps {
+  setResults: (results: { name: string }[]) => void;
+  results: { name: string }[];
+}
+
 function App() {
 
-  const [results, setResults] = useState<[]>([])
+  const [results, setResults] = useState<AppProps['results']>([])
 
   return (
     <div className='App'>
