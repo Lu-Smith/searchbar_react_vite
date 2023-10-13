5422e19fff0fffa3bb6aa92bc8aa1c27acbde85d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-13 17:39:34 +0100

    Updated Footer content

---
+++ react-search-bar/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h4>Coded by <a href="">Luna Smith</a> - <a href="https://github.com/Lu-Smith/searchbar_react_vite"> OPEN SOURCE</a>.</h4>
+        <button>Click me</button>
+    </div>
+  )
+}
+
+export default Footer
