c6099ad555edae1963ec74d9f59bf324ace51ddc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-14 19:35:21 +0100

    Created pages in App.tsx

--- react-search-bar/src/App.tsx
+++ react-search-bar/src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import SearchBar from "./components/SearchBar";
 import SearchResults from './components/SearchResults';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+
+//pages
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Puzzle from './components/pages/Puzzle';
 
 interface AppProps {
   setResults: (results: { name: string }[]) => void;
@@ -17,6 +24,12 @@ function App() {
     <div className='App'>
       <div className="searchBarContainer">
         <NavBar />
+        <Routes>
+        <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/puzzle' element={<Puzzle />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
         <SearchBar setResults={setResults} />
         <SearchResults results={results} />
         <Footer />
