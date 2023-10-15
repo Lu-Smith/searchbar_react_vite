3b3cce52157e32d649a6fea9ed3cc7d9bfa99a13
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-15 12:29:56 +0100

    Updated Footer styling

--- react-search-bar/src/components/Footer.tsx
+++ react-search-bar/src/components/Footer.tsx
@@ -2,10 +2,28 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <h4>Coded by 
-            <a href="https://www.lunasmithart.com/" target='_blank' rel="noreferrer">Luna Smith</a> - 
-            <a href="https://github.com/Lu-Smith/searchbar_react_vite" target='_blank' rel="noreferrer"> OPEN SOURCE</a>.</h4>
+    <div className='relative flex flex-col items-center justify-center h-screen'>
+        <h4 className='position: fixed bottom-20'>Coded by 
+            <a 
+            className='text-green-800 hover:text-blue-800 transition duration-300 ml-1 mr-1 
+            relative font-medium no-underline hover:underline decoration-4 underline-offset-4
+            decoration-green-600 hover:animate-pulse' 
+            href="https://www.lunasmithart.com/" 
+            target='_blank' 
+            rel="noreferrer"> 
+              Luna Smith
+            </a> 
+            - 
+            <a 
+            className='text-green-800 hover:text-blue-800 transition duration-300 ml-1 
+            no-underline hover:underline decoration-2 underline-offset-4 hover:animate-pulse' 
+            href="https://github.com/Lu-Smith/searchbar_react_vite" 
+            target='_blank' 
+            rel="noreferrer"> 
+              OPEN SOURCE
+            </a>
+            .
+        </h4>
         <button 
         className='ring-1 ring-offset-4
         ring-green-800 rounded-md
@@ -13,7 +31,9 @@ const Footer = () => {
          text-white hover:bg-sky-800 
          hover:text-zinc-200 
          sm:px-7 sm:py-2
-         hover:ring-blue-300'
+         hover:ring-blue-300
+         position: fixed bottom-5
+        '
          >Click me</button>
     </div>
   )
