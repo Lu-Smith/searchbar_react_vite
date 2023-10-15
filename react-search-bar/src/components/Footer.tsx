0b28322614ef914d9c07ad2fb100951b29a57bd8
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-15 11:37:10 +0100

    Installed tailwind

--- react-search-bar/src/components/Footer.tsx
+++ react-search-bar/src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <h4>Coded by 
             <a href="https://www.lunasmithart.com/" target='_blank' rel="noreferrer">Luna Smith</a> - 
             <a href="https://github.com/Lu-Smith/searchbar_react_vite" target='_blank' rel="noreferrer"> OPEN SOURCE</a>.</h4>
-        <button>Click me</button>
+        <button className='text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800'>Click me</button>
     </div>
   )
 }
