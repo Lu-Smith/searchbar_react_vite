aaa86bc85549cb448fd33845731cb03357d19f92
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-15 11:08:25 +0100

    Added logo image to NavBar

--- react-search-bar/src/components/NavBar.tsx
+++ react-search-bar/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import Logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
     <div>
-      <Link to="/">logo</Link>
+      <Link to="/"><img src={Logo} alt="Logo" /></Link>
       <ul>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
