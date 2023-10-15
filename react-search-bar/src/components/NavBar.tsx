54a95a73b8c0445d0df483b7387d3bfc624f52c7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-15 13:26:01 +0100

    Updated NavBar styling

--- react-search-bar/src/components/NavBar.tsx
+++ react-search-bar/src/components/NavBar.tsx
@@ -4,13 +4,31 @@ import Logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
-    <div>
-      <Link to="/"><img src={Logo} alt="Logo" /></Link>
-      <ul>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/puzzle">Puzzle</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+    <div 
+    className='bg-slate-200 p-2 flex flex-row justify-between 
+    items-center shadow-md shadow-slate-150'>
+      <Link to="/"><img src={Logo} alt="Logo" className='hover:animate-spin flex-none w-14 h-14'/></Link>
+      <ul className='flex flex-row justify-around items-end gap-2' >
+        <NavLink 
+        to="/" 
+        className='hover:text-pink-400 transition-all duration-200 focus:text-white focus:bg-black p-2 rounded'>
+          Home
+        </NavLink>
+        <NavLink 
+        to="/about" 
+        className='hover:text-pink-400 transition-all duration-200 focus:text-white focus:bg-black p-2 rounded'>
+          About
+        </NavLink>
+        <NavLink 
+        to="/puzzle" 
+        className='hover:text-pink-400 transition-all duration-200 focus:text-white focus:bg-black p-2 rounded'>
+          Puzzle
+        </NavLink>
+        <NavLink 
+        to="/contact" 
+        className='hover:text-pink-400 transition-all duration-200 focus:text-white focus:bg-black p-2 rounded'>
+          Contact
+        </NavLink>
       </ul>
     </div>
   )
