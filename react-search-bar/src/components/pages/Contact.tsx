f77e512b11873cbbab80d39952de75ce66aab784
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-18 23:37:07 +0100

    Updated Contact.tsx content

--- react-search-bar/src/components/pages/Contact.tsx
+++ react-search-bar/src/components/pages/Contact.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='text-center py-4'>Contact</div>
+    <div className='text-center p-4'>
+      <h3>To explore more of my art projects or get in touch, please visit my 
+        <a 
+          className='text-green-800 hover:text-blue-800 transition duration-300 ml-1 mr-1 
+          relative font-medium no-underline hover:underline decoration-4 underline-offset-4
+          decoration-green-600 hover:animate-pulse' 
+          href="https://www.lunasmithart.com/" 
+          target='_blank' 
+          rel="noreferrer"> 
+            portfolio
+        </a> 
+        or contact me at artistlunasmith@gmail.com.
+      </h3>
+    </div>
   )
 }
 
