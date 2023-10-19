895d594553551275da219c5c4548b983005e7cc7
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-19 17:47:10 +0100

    Updated SearchResult

--- react-search-bar/src/components/SearchResults.tsx
+++ react-search-bar/src/components/SearchResults.tsx
@@ -6,9 +6,13 @@ interface ResultsProps {
 
 const SearchResults = ({results}: ResultsProps) => {
   return (
-    <div className='resultsList'>
+    <div className='mt-4 flex flex-col items-center justify-center'>
         {results.map((result, id: number )=> {
-            return <div key={id} onClick={() => alert(`You click on ${result.name}`)}>{result.name}</div>
+            return <div 
+            key={id} 
+            onClick={() => alert(`You click on ${result.name}`)}
+            className=' hover:bg-slate-200 p-2 w-1/2 text-center rounded transition-all duration-200'>{result.name}
+            </div>
         })}
     </div>
   )
