4b22f6fd482da94a2588b8dd6f204a5e6c5b600a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-19 18:25:55 +0100

    Updated content in Puzzle.tsx

--- react-search-bar/src/components/pages/Puzzle.tsx
+++ react-search-bar/src/components/pages/Puzzle.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const Puzzle = () => {
+  const boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
-    <div className='text-center py-4'>Puzzle</div>
-  )
+    <div className='text-center my-4 flex justify-center items-center'>
+    <div className='grid grid-rows-4 grid-flow-col justify-items-center items-center bg-green-800  w-[450px] h-[600px] '>
+      {boxes.map((box, index) => {
+        return (
+          <div
+            key={index}
+            className='bg-red-200 text-white w-[150px] h-[150px] flex justify-center items-center'
+          >
+            <div>{box}</div>
+          </div>
+        );
+      })}
+    </div>
+  </div>
+  );
 }
 
-export default Puzzle
+export default Puzzle;
