f4282cbf5b8090c98ecda3e8d2daa7f8e43aefb6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-10-20 10:42:12 +0100

    Finshed the project

--- react-search-bar/src/components/pages/Puzzle.tsx
+++ react-search-bar/src/components/pages/Puzzle.tsx
@@ -25,20 +25,23 @@ const Puzzle = () => {
   return (
     <div className='text-center my-8 flex flex-col justify-center items-center'>
     <div className={`grid grid-rows-4 grid-flow-col justify-items-center items-center 
-    bg-green-800 w-[450px] h-[600px] ${newPainting ? 'bg-painting2' : 'bg-painting1'} bg-center bg-contain`}>
+    bg-green-800 sm:w-[450px] sm:h-[600px] ${newPainting ? 'bg-painting2' : 'bg-painting1'} bg-center bg-contain`}>
       {boxes.map((box, index) => {
         return (
           <div
             key={index}
             onClick={() => toggleBox(index)}
-            className={` ${openBox[index] ? 'bg-transparent' : 'bg-red-200 border-white border-2'} text-white w-[150px] h-[150px] flex justify-center items-center`}
+            className={` ${openBox[index] ? 'bg-transparent' : 'bg-red-200 border-white border-2'} 
+            text-white sm:w-[150px] sm:h-[150px] w-[100px] h-[100px] flex justify-center items-center`}
           >
             <div className='text-xl'>{box}</div>
           </div>
         );
       })}
     </div>
-    <button onClick={handleNewPainting}>New Puzzle</button>
+    <button 
+    onClick={handleNewPainting} 
+    className='hover:text-red-200 transition-all duration-200'>New Puzzle</button>
   </div>
   );
 }
